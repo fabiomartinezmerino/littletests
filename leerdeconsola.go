23a91bcf910d5631feed228b1425df14e83f3936
fabsenberg <FABIOMARTINEZMERINO@GMAIL.COM>
2024-03-26 18:25:51 +0100

    Primera prueba

--- leerdeconsola.go
+++ leerdeconsola.go
@@ -1,15 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"unicode/utf8"
+)
 
 func main() {
 
-	var i int
-	_, err := fmt.Scanf("Input a number %d", &i)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter text: ")
+	text, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Printf("El número introducido es: %d", i)
+		log.Fatalf("%v\n", err)
 	}
+	text = strings.Trim(text, "\n")
+	fmt.Printf("The text is %v characters long\n", utf8.RuneCountInString(text))
+
+	sl := strings.Split(text, " ")
+
+	sl = append(sl, "!!!")
+	output := strings.Join(sl, " ")
+	fmt.Printf("Salida: %s\n", output)
 
 }
